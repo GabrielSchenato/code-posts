52d4de006793c714db94a1a624da927db87f857a
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-20 17:45:02 -0300

    Autorização com middleware

--- src/CodePosts/Controllers/AdminPostsController.php
+++ src/CodePosts/Controllers/AdminPostsController.php
@@ -16,87 +16,72 @@ class AdminPostsController extends Controller
 
     private $response;
     private $repository;
-    
+
     public function __construct(ResponseFactory $response, PostRepositoryInterface $repository)
     {
         $this->repository = $repository;
         $this->response = $response;
     }
-    
+
     public function index()
     {
-        $this->auth();
         $posts = $this->repository->all();
         return $this->response->view('codepost::index', compact('posts'));
     }
-    
+
     public function show(int $id)
     {
-        $this->auth();
         $post = $this->repository->find($id);
         return $this->response->view('codepost::show', compact('post'));
     }
-    
+
     public function create()
     {
-        $this->auth();
         return $this->response->view('codepost::create');
     }
-    
+
     public function store(Request $request)
     {
-        $this->auth();
         $this->repository->create($request->all());
-        
+
         return redirect()->route('admin.posts.index');
     }
-    
+
     public function edit(int $id)
     {
-        $this->auth();
         $post = $this->repository->find($id);
         return $this->response->view('codepost::edit', compact('post'));
     }
-    
+
     public function update(int $id, Request $request)
     {
-        $this->auth();
         $this->repository->update($request->all(), $id);
-        
+
         return redirect()->route('admin.posts.index');
     }
-    
+
     public function destroy(int $id)
     {
-        $this->auth();
         $this->repository->find($id)->delete();
         return redirect()->route('admin.posts.index');
     }
-    
+
     public function deleted()
     {
-        $this->auth();
         $posts = $this->repository->deleted();
         return $this->response->view('codepost::deleted', compact('posts'));
     }
-    
+
     public function restore(int $id)
     {
-        $this->auth();
         $this->repository->restore($id);
         return redirect()->route('admin.posts.index');
     }
-    
+
     public function updateState(Request $request, int $id)
     {
-        $this->authorize('publish_post');
         $this->repository->updateState($id, $request->get('state'));
         return redirect()->route('admin.posts.edit', ['id' => $id]);
-    }
-    
-    private function auth()
-    {
-        $this->authorize('access_posts');
     }
 
 }
--- src/CodePosts/routes.php
+++ src/CodePosts/routes.php
@@ -2,12 +2,12 @@
 
 Route::name('admin.')
         ->prefix('admin/')
-        ->middleware('web', 'auth')
+        ->middleware('web', 'auth', 'authorization:access_posts')
         ->namespace('CodePress\CodePosts\Controllers')
         ->group(function () {
             Route::get('posts/deleted', 'AdminPostsController@deleted')->name('posts.deleted');
             Route::get('posts/deleted/restore/{post}', 'AdminPostsController@restore')->name('posts.restore');
-            Route::patch('posts/update-state/{post}', 'AdminPostsController@updateState')->name('posts.update_state');
+            Route::patch('posts/update-state/{post}', 'AdminPostsController@updateState')->name('posts.update_state')->middleware('authorization:publish_post');
             Route::resources([
                 'posts' => 'AdminPostsController'
             ]);
